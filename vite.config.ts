a4840a00750af5d1755225d3e383a4925b29b252
RabdiyaProductions <kiranrabdiya@gmail.com>
2025-12-23 09:36:24 +0000

    Added GIT deploy page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flowform-hub-pwa/',
-})
+  base: "/flowform_hub_pwa/",
+});
